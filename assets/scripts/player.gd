5a90b0e5a201d5185aaa76fc8097ce96fcfb92de
CheesyDeLite <130111543+CheesyDeLite@users.noreply.github.com>
2025-11-26 21:33:28 +1100

    added basic physics/movement

--- assets/scripts/player.gd
+++ assets/scripts/player.gd
@@ -1,19 +1,22 @@
 extends CharacterBody2D
 
 @export var base_speed: int
+@export var rotation_speed: int
 var speed: int
+const GRAVITY = 900
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	speed = base_speed
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	movement()
+	movement(delta)
 
 
-func movement():
+func movement(delta):
 	var direction = Input.get_axis("left", "right")
 	velocity.x = direction * speed
+	rotation_degrees += direction * rotation_speed
+	velocity.y += GRAVITY * delta
 	move_and_slide()
